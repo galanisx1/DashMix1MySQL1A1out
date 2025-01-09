5e21e680b190fe6389a8836a3d1f4e44b4daa629
galanisx1 <100341218+galanisx1@users.noreply.github.com>
2025-01-09 07:53:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -89,4 +89,4 @@ def get_dinero_data():
     return jsonify({'data': data, 'total_ventas': total_ventas})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
